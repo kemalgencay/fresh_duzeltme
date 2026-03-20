77c68bffe68acb448820caaf03d0203db14e34db
kemalgencay <kemalgencay@gmail.com>
2026-03-20 08:18:06 +0300

    first

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,17 +1,16 @@
-import { define } from "../utils.ts";
-
-export default define.page(function App({ Component }) {
+import { type PageProps } from "fresh";
+export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>fresh_duzeltme</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <title>Kemal'in sitesi</title>
+        <link rel="stylesheet" href="/static/styles.css" />
       </head>
       <body>
         <Component />
       </body>
     </html>
   );
-});
+}
